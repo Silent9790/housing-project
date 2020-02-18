8a8a2499dce03ed8d0f44f0856dd0f046e8d7597
Tasi Pedro <Tpedro1117@gmail.com>
2020-02-18 11:11:17 -0800

    jumbo comp TP

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,25 +1,12 @@
 import React from "react";
+import TeamJumbotron from "../components/TeamJumbotron";
 
 function Team() {
-
     return (
         <div>
-          <h1>Team</h1>
+          {/* <h1>Location</h1> */}
+          <Team Jumbotron />
         </div>
       );
 }
-
-
-    export default Team;
-
-
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//     <a className="navbar-brand" href="/">
-//       Your personal CMS
-//     </a>
-//     {/* Display this if the current state is loading */}
-//     {/* <a className="navbar-brand ml-auto">
-//         Loading...
-//       </a> */}
-//   </nav>
-// );
+export default Team;
