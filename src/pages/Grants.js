35ff2c32a465c0995360e9396bff6dbbda5ef2ff
Tasi Pedro <Tpedro1117@gmail.com>
2020-02-18 14:13:57 -0800

    pdf links

--- src/pages/Grants.js
+++ src/pages/Grants.js
@@ -13,3 +13,5 @@ function Grants() {
 }
 
 export default Grants;
+//Below is the link for PDF file on its own  on GoogleDrive 
+// https://drive.google.com/drive/folders/1xOQpGQW1R9FhgfPwztx7ehPvlwi8Mh6H?usp=sharing
