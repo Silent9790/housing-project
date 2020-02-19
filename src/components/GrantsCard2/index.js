ec3931025eff09881b253808183cda3d49b52940
Tasi Pedro <Tpedro1117@gmail.com>
2020-02-19 12:11:23 -0800

    grantcards TP

---
+++ src/components/GrantsCard2/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function GrantsCard2 () {
+    return (
+        <div className = "row">
+        <div className="col s1 m3">
+            <div class="card">
+                <div className = "card-image"> 
+             <h2 className="header">Home Repair </h2>
+            {/* <div className="card horizontal"> */}
+                {/* <div className="card-image"> */}
+                <img src="https://tse2.mm.bing.net/th?id=OIP.G7N-Q8p8bpODtPOuKxrEXAHaDf&pid=Api&P=0&w=347&h=166" alt="fall" />
+                 <span class = "card-title">Home Repair </span>
+                 {/* <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a> */}
+                 </div>
+                    <div className="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="https://drive.google.com/file/d/1KCCTyYqQdzCqjmdpA4R9yy6CaeqyDyZk/view?usp=sharing">Apply Now!</a>
+                    </div>
+                </div>
+                {/* </div> */}
+            </div> 
+            </div>
+    );
+}
+export default GrantsCard2;
--- src/components/GrantsJumbotron/index.js
+++ src/components/GrantsJumbotron/index.js
@@ -1,12 +1,25 @@
 import React from "react";
+import GrantsCarousel from "../GrantsCarousel";
+import GrantsCard1 from "../GrantsCard3";
+import GrantsCard2 from "../GrantsCard2";
+import GrantsCard3 from "../GrantsCard1";
+// import GrantsCard4 from "../GrantsCard4";
+
+
 
 function GrantsJumbotron({ children }) {
   return (
-    <div style={{ height: 800, clear: "both", paddingTop: 120, textAlign: "center", backgroundColor: "tan" }}
+    <div style={{ height: 2000, clear: "both", paddingTop: 50, textAlign: "center", backgroundColor: "tan" }}
       className="jumbotron"
     >
-      {children}
+      {/* <GrantsCarousel /> */}
+      <GrantsCard3 />
+      <GrantsCard2 />
+      <GrantsCard1 />
+      {/* <GrantsCard4 /> */}
+      {/* <GrantsCard5 /> */}
     </div>
+
   );
 }
 
