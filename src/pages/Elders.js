d7a9a4b3e43daa52da9a139e4acd5754522f6644
Phillip Dunham <phillipwwd@gmail.com>
2020-02-19 09:38:39 -0800

    No progress, need to change branches

--- src/pages/Elders.js
+++ src/pages/Elders.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Axios from "axios";
 
 function Elders() {
+
+  const getPDFHousingProgram = () => {
+    const URL = "https://pdfhost.io/v/.UQ~tXPa_MENU_EL_GATO_2020_UKpdf.pdf"
+
+    Axios.get(
+      URL
+    ).then(response => {
+      console.log(response)
+    })
+  }
+
   return (
     <div>
       <h1>Elders</h1>
+      <button onClick = {getPDFHousingProgram}>Get PDF</button>
     </div>
   );
 }
