57974f0c54d32d1edda83a2f260208ed79689ba1
Aidan Willett <Gamer9310@gmail.com>
2020-02-19 20:36:35 -0800

    working on footer

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import ImageGallery from 'react-image-gallery';
 import M from "materialize-css";
 import "./style.css";
 import Carousel1 from "../../pictures/grass.jpg";
@@ -15,24 +14,15 @@ export default class Carousel extends Component {
         });
     }
 
+    
+
     render(){
         return (
             <div className="carousel carousel-slider">
-                <div className="carousel-background">
-                    
-                </div>
-                <div className="image1">
                 <a className="carousel-item" href="#one!"><img src={Carousel1} alt="Image1" /></a>
-                </div>
-                {/* <div className="image2">
                 <a className="carousel-item" href="#two!"><img src={Carousel2} alt="Image2" /></a>
-                </div>
-                <div className="image3">
                 <a className="carousel-item" href="#three!"><img src={Carousel3} alt="Image3" /></a>
-                </div>
-                <div className="image4">
                 <a className="carousel-item" href="#four!"><img src={Carousel4} alt="Image4" /></a>
-                </div> */}
             </div>
         );
     }
