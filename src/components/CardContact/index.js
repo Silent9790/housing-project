7c7dd4193cd1bd52886d6409a9635508a71f723c
Aidan Willett <Gamer9310@gmail.com>
2020-02-20 10:46:26 -0800

    lending

--- src/components/CardContact/index.js
+++ src/components/CardContact/index.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { BrowserRouter as Route, Link } from "react-router-dom";
 
-function CardContact(){
-
-return(
-<div class="col s12">
-<div class="card horizontal">
-  <div class="card-stacked">
-  <div class="card-content center-align">
-      <h5>Need to get in touch?</h5>
-    </div>
-    <div class="card-content">
-      <p>If you Want some help, or you eve have questions, click here to get in contact with us</p>
-    </div>
-    <div class="card-action">
-    </div>
-  </div>
-</div>
-</div>)
-        }
+function CardContact() {
+  return (
+    <Link to="/contact">
+      <div className="col s12" className="black-text">
+        <div className="card horizontal">
+          <div className="card-stacked">
+            <div className="card-content center-align">
+              <h5 className="black-text">Need to get in touch?</h5>
+            </div>
+            <div className="card-content">
+              <p>
+                If you Want some help, or you even have questions, click here to
+                get in contact with us.
+              </p>
+            </div>
+            <div className="card-action"></div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
 export default CardContact;
