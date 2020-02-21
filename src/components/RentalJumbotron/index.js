465e56cd989556947879742b24e9ec517fe8b66f
Phillip Dunham <phillipwwd@gmail.com>
2020-02-21 14:39:30 -0800

    Adding responsive styling to rentals.

--- src/components/RentalJumbotron/index.js
+++ src/components/RentalJumbotron/index.js
@@ -9,10 +9,12 @@ function RentalJumbotron({ children }) {
     >
       {children}
       <div id="jumboBar">
-        <img id="rentalImg" src={rentalImg} alt="Rental" />
-        <h3 id="rentalTitle">Rental Housing Program</h3>
-        <p id="rentalHeadingText">Visit one of our 302 rental properties within or near the reservation.</p>
-        <a href="https://drive.google.com/file/d/1ni7SXj7NiSVokzoPO3eUL5sQ-k6-U3Xq/view?usp=sharing"  rel="noopener noreferrer" target="_blank"><button id="applyNow">Apply Now</button></a>
+        <img id="rentalImg" src={rentalImg} alt="Rental" style = {{width: "40vw", height: "22vw" }}/>
+        <div className = "titleDiv">
+          <h3 id="rentalTitle">Rental Housing Program</h3>
+          <p id="rentalHeadingText">Visit one of our 302 rental properties within or near the reservation.</p>
+          <a href="https://drive.google.com/file/d/1ni7SXj7NiSVokzoPO3eUL5sQ-k6-U3Xq/view?usp=sharing"  rel="noopener noreferrer" target="_blank"><button id="applyNow">Apply Now</button></a>
+        </div>
       </div>
     </div>
   );
