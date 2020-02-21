fd0eddb2f3e52a68a1f7d8940d5feab7b199ab9d
Tasi Pedro <Tpedro1117@gmail.com>
2020-02-20 19:05:50 -0800

    maintp

---
+++ src/components/MaintenanceCards/index.js
@@ -0,0 +1,96 @@
+import React from "react";
+// import "./style.css";s
+import Woman from "../../pictures/maintenance1.jpeg"
+import One from "../../pictures/emergency1.jpeg"
+import Two from "../../pictures/emergency2.jpeg"
+import Three from "../../pictures/emergency3.jpeg"
+import Four from "../../pictures/emergency4.jpeg"
+
+
+function MaintenanceCards() {
+    return (
+        <div className="container">
+            <h5 id="teamIntroText">Departments</h5>
+
+            {/* <div className="row">
+                <div className="col s6 m6">
+                    <div className="card horizontal">
+                        <img className="maintenanceCategorycircle" src={Woman} alt="profile" />
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <a href="https://www.google.com/">Unit Turn</a>
+                                <p>Email: john@bomb.com</p>
+                                <p>Phone: (111) 111-1111</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col s6 m6">
+                    <div className="card horizontal">
+                        <img className="teamProfileImg circle" src={Woman} alt="profile" />
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <a href="https://www.google.com/">Rental Maintenance</a>
+                                <p>Email: jack@jack.jack</p>
+                                <p>Phone: (111) 111-1112</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+            <div className="col s6 m6">
+                    <div className="card horizontal red darken-4">
+                        <img className="teamProfileImg circle" src={Four} alt="profile" />
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <a href="https://www.google.com/">Emergency</a>
+                                <p>Email: tas@jp.tas</p>
+                                <p>Phone: (000) 456 - 1238</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+
+                <div className="col s6 m6">
+                    <div className="card horizontal teal lighten-2 ">
+                        <img className="teamProfileImg circle" src={Two} alt="profile" />
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <a href="https://www.google.com/">Unit Turn</a>
+                                <p>Email: philliam@phil.phillip</p>
+                                <p>Phone: (123) 456-7890</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col s6 m6">
+                    <div className="card horizontal teal lighten-2">
+                        <img className="teamProfileImg circle" src={Three} alt="profile" />
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <a href="https://www.google.com/">Rental Maintenance</a>
+                                <p>Email: nick@seto.nicks</p>
+                                <p>Phone: (000) 456 - 1234</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col s6 m6">
+                    <div className="card horizontal teal lighten-2">
+                        <img className="teamProfileImg circle" src={One} alt="profile" />
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <a href="https://www.google.com/">Construction</a>
+                                <p>Email: aiden@den.aiden</p>
+                                <p>Phone: (000) 456 - 1235</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+         </div>
+    );
+}
+
+export default MaintenanceCards;
