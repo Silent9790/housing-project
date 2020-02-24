178a0fbb18ab1748441a8c8990fb6bac2007a06a
Phillip Dunham <phillipwwd@gmail.com>
2020-02-24 14:13:16 -0800

    Tweaking buttons.

--- src/components/LoginCard/index.js
+++ src/components/LoginCard/index.js
@@ -3,39 +3,51 @@ import "./style.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function LoginCard() {
-    return (
-        <div className="container">
-            <h5 className="loginTitle">Login to your MHA Account:</h5>
-            <div className="row">
-                <div className="col s8 offset-s2">
-                    <div className="card red darken-4 loginCard">
-                        <div className="card-content white-text">
-                            <form className="container">
-                                <div className="row">
-                                    <x-h7 className="white-text">User ID:</x-h7>
-                                    <div className="input-field col m12 white">
-                                        <input className="validate" type="text" />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <x-h7 className="white-text">Password:</x-h7>
-                                    <div className="input-field col m12 white">
-                                        <input className="validate" type="password" />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <input className="loginButton" type="button" value="Login" />
-                                    <Link to="/createaccount"><input className="loginButton" type="button" value="Create Account" /></Link>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+  return (
+    <div className="container">
+      <h5 className="loginTitle">Login to your MHA Account:</h5>
+      <div className="row">
+        <div className="col s8 offset-s2">
+          <div className="card red darken-4 loginCard">
+            <div className="card-content white-text">
+              <form className="container">
+                <div className="row">
+                  {/* <x-h7 className="white-text">User ID:</x-h7> */}
+                  <label for="userID" className="white-text">
+                    User ID:
+                  </label>
+                  <div className="input-field col m12 white">
+                    <input className="validate" type="text" name="userID" />
+                  </div>
                 </div>
+                <div className="row ">
+                    <label for = "password" className = "white-text">Password:</label>
+                  {/* <x-h7 className="white-text">Password:</x-h7> */}
+                  <div className="input-field col m12 white">
+                    <input className="validate" type="password" name = "password"/>
+                  </div>
+                </div>
+                <div className="row center-align" id="buttonsBox">
+                    <div className=" offset-s2">
+
+                    </div>
+                  <input className="loginButton" type="button" value="Login"                       id="loginlog"/>
+                  <Link to="/createaccount">
+                    <input
+                      className="loginButton"
+                      type="button"
+                      value="Create Account"
+                    />
+                  </Link>
+                </div>
+              </form>
             </div>
-            <div id="loginBumper">
-            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div id="loginBumper"></div>
+    </div>
+  );
 }
 
 export default LoginCard;
